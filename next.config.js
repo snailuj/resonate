ec56b29a23b814a64833f8d9cbf7fbcf26850f1d
Julian Suggate <julian.suggate@gmail.com>
2019-04-19 23:19:44 +1200

    Add settings to Nextjs config for publishing to github pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const withCSS = require('@zeit/next-css')
+
+const isProd = process.env.NODE_ENV == "production"
 module.exports = withCSS({
   webpack: (config, { buildId, dev }) => {
     config.resolve.symlinks = true
     return config
   }
+  , assetPrefix: isProd ? 'https://snailuj.github.io' : ''
 })
