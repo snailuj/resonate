7a96ca3f2eb83fc3054366e079c1ba61b4301794
Julian Suggate <julian.suggate@gmail.com>
2019-04-19 23:40:43 +1200

    Change links to be more descriptive

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ const Home = () => {
   return <>
     <div className="container">
       <nav>
+        <Link prefetch href="full"><a>Full Resume</a></Link>
         <Link prefetch href="blank"><a>Blank Sample</a></Link>
-        <Link prefetch href="full"><a>Full Sample</a></Link>
       </nav>
     </div>
   </>
